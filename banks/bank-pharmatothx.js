2ff8fb0a946112cee11e9272203837e15dab5c3f
imkyuuuuu <imkyuuuuu@gmail.com>
2026-02-01 18:32:54 -0500

    Update bank-pharmatothx.js

--- banks/bank-pharmatothx.js
+++ banks/bank-pharmatothx.js
@@ -1,1 +1,429 @@
+window.QUIZ_BANKS = window.QUIZ_BANKS || {};
 
+window.QUIZ_BANKS["bank-pharmatothx"] = {
+  label: "Pharmacothérapie (2021-2025)",
+  questions: [
+    // ===================== 2025 =====================
+    {
+      id: "phm_2025_01",
+      stem: "Comment la mirtazapine augmente-t-elle la sérotonine ?",
+      choices: ["Inhibiteur transporteur sérotonine", "Blocage/Antagoniste 5 HT-2a et 5 HT-2c", "Antagoniste alpha 2-adrénergique"],
+      answerIndex: 2,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_02",
+      stem: "Différence entre méthylphénidate et amphétamine ?",
+      choices: ["Amphétamine joue sur VMAT", "Méthylphénidate augmente relargage DA/NA en présynaptique", "Méthylphénidate bloque recapture DA/NA", "Méthylphénidate agit sur 5HT1a"],
+      answerIndex: 0,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_03",
+      stem: "Différence entre IMAO-A et IMAO-B ?",
+      choices: ["IMAO-B dégrade DA/Histamine ; IMAO-A dégrade 5HT/NE/DA", "IMAO-A est mitochondrial ; IMAO-B est cytoplasmique", "IMAO-A est inhibé de façon irréversible ; IMAO-B réversible"],
+      answerIndex: 0,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_04",
+      stem: "Quel psychédélique comprend un effet IMAO ?",
+      choices: ["LSD", "Psilocybine", "Mescaline", "Ayahuasca"],
+      answerIndex: 3,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_05",
+      stem: "Agriculteur sous lithium, chaleur intense. Ataxie, confusion, tremblement, déshydratation. Diagnostic ?",
+      choices: ["Délirium", "Encéphalopathie", "Intoxication au Lithium", "Intoxication à l’alcool"],
+      answerIndex: 2,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_06",
+      stem: "Patient MAB 1 sous lithium, déshydraté, dysarthrique et confus. Action ?",
+      choices: ["Baisser lithium", "Baisser gabapentin"],
+      answerIndex: 0,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_07",
+      stem: "Patient sous clozapine remplace cigarette par vapotage. Devient léthargique avec hypersialorrhée. Cause ?",
+      choices: ["Infection 2aire neutro bas", "Consommation cannabis", "Cigarette remplacée par vapotage", "Intoxication alcoolique"],
+      answerIndex: 2,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_08",
+      stem: "Que mesure spécifiquement le test MMPI ?",
+      choices: ["Test projectif", "Mesure introversion sociale", "Fonctionnement adaptatif"],
+      answerIndex: 1,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_09",
+      stem: "Théorie de l'idéalisation des parents avec le thérapeute pour compléter un besoin développemental ?",
+      choices: ["Kohut", "Kernberg", "Kahlbaum", "Autre"],
+      answerIndex: 0,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_10",
+      stem: "Pourquoi les personnes âgées font-elles plus d'HTO ?",
+      choices: ["Changement sensibilité récepteurs", "Changement masse adipeuse", "Changement filtration glomérulaire"],
+      answerIndex: 0,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_11",
+      stem: "Chez quel patient faut-il être le plus vigilant avec l’acamprosate ?",
+      choices: ["Insuffisance hépatique", "Insuffisance rénale", "65 ans et plus"],
+      answerIndex: 1,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_12",
+      stem: "Traitement première ligne pour TAG chez patiente de 72 ans ?",
+      choices: ["Duloxétine", "Sertraline", "Prégabaline"],
+      answerIndex: 2,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_13",
+      stem: "Mécanisme d'action du lemborexant ?",
+      choices: ["Antagoniste récepteurs orexine"],
+      answerIndex: 0,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_14",
+      stem: "Facteur de risque le plus important de dyskinésie tardive chez femme avec SOPK ?",
+      choices: ["Âge", "Sexe", "SOPK", "Insulinorésistance"],
+      answerIndex: 3,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_15",
+      stem: "Risque prioritaire à mentionner pour cannabis chez patient âgé avec Parkinson/Vasc/MPOC ?",
+      choices: ["Bronchite chronique", "Ostéoporose", "AVC", "Trouble moteur"],
+      answerIndex: 2,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_16",
+      stem: "TCC-I, séance 5. Efficacité sommeil 93% (22h30-6h00). Prochaine étape ?",
+      choices: ["Coucher 22h15", "Lever 5h45", "Même horaire", "Lever 6h15"],
+      answerIndex: 3,
+      tags: ["2025", "sommeil"]
+    },
+    {
+      id: "phm_2025_17",
+      stem: "Patient agité ++, reçoit plusieurs Rx. Douleur/raideur diffuse, urine couleur thé. SV/Conscience N. Diagnostic ?",
+      choices: ["Dystonie", "SNM", "Rhabdomyolyse", "Syndrome sérotoninergique"],
+      answerIndex: 2,
+      tags: ["2025", "pharmacologie"]
+    },
+    {
+      id: "phm_2025_18",
+      stem: "But de la thérapie pour trouble dissociatif de l'identité ?",
+      choices: ["Intégrer les identités", "Se choisir une identité", "Créer nouvelle identité englobante"],
+      answerIndex: 0,
+      tags: ["2025", "pharmacologie"]
+    },
+
+    // ===================== 2024 =====================
+    {
+      id: "phm_2024_01",
+      stem: "Quel Rx augmente la concentration de clomipramine par interaction ?",
+      choices: ["Fluvoxamine", "Sertraline", "Citalopram", "Paroxétine"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_02",
+      stem: "Quel neurotransmetteur est modulé par la lamotrigine ?",
+      choices: ["Glutamate", "GABA", "Sérotonine", "Noradrénaline"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_03",
+      stem: "Mécanisme de la mirtazapine pour augmenter la norépinéphrine ?",
+      choices: ["Antagonisme alpha 2", "Agoniste 5ht2c", "Inhibition recapture NE"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_04",
+      stem: "Tx TPL trans sous Testo avec irritabilité, thérapie inefficace. Aide pharmaco ?",
+      choices: ["Fluoxétine", "Lamotrigine", "Lorazépam", "Propranolol"],
+      answerIndex: 1,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_05",
+      stem: "Quel Rx peut nuire à l’efficacité d’un contraceptif oral ?",
+      choices: ["Carbamazépine", "Épival", "Lithium"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_06",
+      stem: "Début clozapine. Retrait social, écholalie, échopraxie, négativisme. Action prioritaire ?",
+      choices: ["Lorazépam", "Cesser clozapine", "Augmenter clozapine", "Diminuer clozapine"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_07",
+      stem: "Femme 80 ans sous citalopram 40 mg. Action prioritaire ?",
+      choices: ["ECG", "Électrolytes"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_08",
+      stem: "Effet secondaire le plus fréquent de la cariprazine ?",
+      choices: ["Akathisie", "Anxiété", "Diarrhée"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_09",
+      stem: "Patient sous Vyvanse et Abilify, développe jeu pathologique. Action ?",
+      choices: ["Diminuer abilify", "Diminuer vyvanse", "Ajouter ISRS"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_10",
+      stem: "Niveau clozapine très élevé chez patient qui a repris tabac et a eu COVID-19. Cause ?",
+      choices: ["Reprise tabac", "Citalopram", "COVID-19", "Clonazépam"],
+      answerIndex: 2,
+      tags: ["2024", "pharmacologie"]
+    },
+    {
+      id: "phm_2024_11",
+      stem: "Effet secondaire fréquent de la sertraline chez femme de 80 ans ?",
+      choices: ["Hyponatrémie", "Hypothyroïdie", "Thrombocytose", "HTO"],
+      answerIndex: 0,
+      tags: ["2024", "pharmacologie"]
+    },
+
+    // ===================== 2023 =====================
+    {
+      id: "phm_2023_01",
+      stem: "Réaction grave possible sous Tétrabénazine ?",
+      choices: ["Manie", "Psychose", "Dépression/Idées suicidaires"],
+      answerIndex: 2,
+      tags: ["2023", "pharmacologie"]
+    },
+    {
+      id: "phm_2023_02",
+      stem: "Rx causant pertes de mémoire/sx cognitifs en combinaison ?",
+      choices: ["Topiramate", "AINS", "Rx physiques"],
+      answerIndex: 0,
+      tags: ["2023", "pharmacologie"]
+    },
+    {
+      id: "phm_2023_03",
+      stem: "Vyvanse (TDAH) + Abilify (SCZ) : développe dépenses compulsives. Action ?",
+      choices: ["Diminuer Aripiprazole", "Diminuer Lisdexamphétamine"],
+      answerIndex: 0,
+      tags: ["2023", "pharmacologie"]
+    },
+
+    // ===================== 2022 =====================
+    {
+      id: "phm_2022_01",
+      stem: "Anxiété après arrêt BZD de longue date. Cause ?",
+      choices: ["Rebond anxiété", "Sevrage aux benzo", "Dépendance BZD"],
+      answerIndex: 1,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_02",
+      stem: "Quel Rx est le plus influencé par l'arrêt tabagique ?",
+      choices: ["Risperdal", "Quétiapine", "Lurasidone", "Olanzapine"],
+      answerIndex: 3,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_03",
+      stem: "Contre-indication ECG absolue au lithium ?",
+      choices: ["Bloc AV 1er degré", "Maladie du nœud sinusal/bradycardie", "Tachycardie supraventriculaire", "QT 450 msec"],
+      answerIndex: 1,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_04",
+      stem: "Patient sous clozapine (dose haussée) avec 2 convulsions. Traitement ?",
+      choices: ["Clonazépam", "Lyrica", "Épival", "Carbamazépine"],
+      answerIndex: 2,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_05",
+      stem: "Effet secondaire dose-dépendant le plus inquiétant de la clozapine ?",
+      choices: ["Convulsion", "Sialorrhée", "Agranulocytose", "Myocardite"],
+      answerIndex: 0,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_06",
+      stem: "Présentation la plus commune chez nouveau-nés mères sous ISRS ?",
+      choices: ["Syndrome inadaptation néonatale", "HTP persistante", "Malformations cardiaques"],
+      answerIndex: 0,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_07",
+      stem: "Antidépresseur à privilégier chez patient avec risque de saignement ?",
+      choices: ["Wellbutrin", "Effexor XR", "Paxil", "Celexa"],
+      answerIndex: 0,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_08",
+      stem: "Antidépresseur privilégié pour EDC avec crainte effets sexuels ?",
+      choices: ["Wellbutrin", "Effexor XR", "Sertraline", "Celexa"],
+      answerIndex: 0,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_09",
+      stem: "Spasme cou après cocaïne + Rispéridone. Traitement ?",
+      choices: ["Cogentin", "Ativan", "Propranolol", "Dantrolène"],
+      answerIndex: 0,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_10",
+      stem: "Âgée, lithémie 1.4, culture urine 'flore mixte'. Action ?",
+      choices: ["Suspendre lithium", "Refaire lithémie", "Refaire culture", "Hospitaliser"],
+      answerIndex: 0,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_11",
+      stem: "Voie administration Kétamine avec plus haut niveau évidence ?",
+      choices: ["IN", "IV", "PO", "IM"],
+      answerIndex: 1,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_12",
+      stem: "BZD la plus sécuritaire en insuffisance hépatique ?",
+      choices: ["Temazepam", "Alprazolam", "Clonazepam", "Bromazepam"],
+      answerIndex: 0,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_13",
+      stem: "Tremblement vertical bouche sous Abilify 15mg. Action ?",
+      choices: ["Tetrabenazine", "Propranolol", "Clonazépam", "Cogentin"],
+      answerIndex: 2,
+      tags: ["2022", "pharmacologie"]
+    },
+    {
+      id: "phm_2022_14",
+      stem: "Remplacer Paroxétine (mauvaise compliance/sevrage) pour éviter situation ?",
+      choices: ["Fluoxétine", "Venlafaxine XR", "Citalopram", "Sertraline"],
+      answerIndex: 0,
+      tags: ["2022", "pharmacologie"]
+    },
+
+    // ===================== 2021 =====================
+    {
+      id: "phm_2021_01",
+      stem: "Abilify IM + TRN : agitation et insomnie. Action ?",
+      choices: ["Bupropion", "Abilify PO", "Loxapine", "Diminuer Abilify IM"],
+      answerIndex: 3,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_02",
+      stem: "Clozapine + arrêt tabac réussi sous TRN. Action ?",
+      choices: ["Diminuer clozapine", "Augmenter clozapine", "Diminuer pendant TRN", "Pas de modif"],
+      answerIndex: 0,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_03",
+      stem: "Mécanisme d'action Lyrica ?",
+      choices: ["Agoniste GABA-A", "Antagoniste NMDA", "Blocage canaux calciques V-D", "Blocage canaux sodium V-D"],
+      answerIndex: 2,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_04",
+      stem: "Mécanisme d'action Varénicline ?",
+      choices: ["Antagoniste nicotinique", "Agoniste partiel nicotinique", "Antagoniste muscarinique"],
+      answerIndex: 1,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_05",
+      stem: "Lequel a le plus d'effets secondaires noradrénergiques ?",
+      choices: ["Duloxétine", "Levomilnacipran", "Venlafaxine", "Desvenlafaxine"],
+      answerIndex: 1,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_06",
+      stem: "Quel AD nécessite ajustement en IRC sévère ?",
+      choices: ["Venlafaxine", "Vortioxétine", "Vilazodone", "Sertraline"],
+      answerIndex: 0,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_07",
+      stem: "Intensité stimulus pour ECT unilatéral pulsatif bref ?",
+      choices: ["2.5x seuil", "4x seuil", "6x seuil", "Au seuil"],
+      answerIndex: 2,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_08",
+      stem: "Que faire pour prolonger convulsion ECT (10 sec actuellement) ?",
+      choices: ["Augmenter propofol", "Ajouter kétamine", "Midazolam matin", "Réduire ondansetron"],
+      answerIndex: 1,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_09",
+      stem: "Vomissement clozapine 1h après dose 100mg (dose coucher 300mg prévue). Action ?",
+      choices: ["Reprendre titration", "Pas reprendre dose souper, prendre dose coucher", "Redonner dose souper", "Donner 50mg"],
+      answerIndex: 1,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_10",
+      stem: "Mécanisme d'action de la kétamine ?",
+      choices: ["Antagoniste NMDA", "Antagoniste AMPA"],
+      answerIndex: 0,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_11",
+      stem: "Sevrage Paroxétine UR (paresthésie, nausée, insomnie). Action ?",
+      choices: ["Réassurance", "Cyproheptadine", "Physostigmine", "Bromocriptine"],
+      answerIndex: 0,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_12",
+      stem: "Psoriasis (éruption surfaces extenseurs). Quel Rx associé ?",
+      choices: ["Lithium", "Lamotrigine", "Épival", "Carbamazépine"],
+      answerIndex: 0,
+      tags: ["2021", "pharmacologie"]
+    },
+    {
+      id: "phm_2021_13",
+      stem: "Lamotrigine ajoutée à Épival + Propranolol. Inquiétude majeure ?",
+      choices: ["Éruption cutanée", "Tremblement", "Dystonie", "Allongement QTc"],
+      answerIndex: 0,
+      tags: ["2021", "pharmacologie"]
+    }
+  ]
+};
